dd60aaf6040343f7c5b70ca66a6d00bafdf99be0
danae-pan <danai231@windowslive.com>
2025-01-22 15:27:57 +0100

    feat: convergence

--- matmult_mkn_omp.cpp
+++ matmult_mkn_omp.cpp
@@ -3,16 +3,13 @@
 extern "C" {
     #include <omp.h>
     
-    
-
-    # pragma omp parallel for 
     void matmult_mkn(int m,int n,int k,double **A,double **B,double **C) {
         double start = omp_get_wtime();
-        for(int i = 0; i < m*n; i++) (*C)[i] = 0;
-
+        #pragma omp parallel for 
         for(int row = 0; row < m; row++) {
-            for(int i = 0; i < k; i++) {
-                for(int col = 0; col < n; col++) {
+            for(int col = 0; col < n; col++) {
+                C[row][col] = 0;
+                for(int i = 0; i < k; i++) {  
                     C[row][col] += A[row][i] * B[i][col];
                 }
             }
@@ -20,6 +17,7 @@ extern "C" {
         double end = omp_get_wtime();
         printf("Execution time %f", end - start);
     }
+
 
     
     
